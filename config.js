2924ca621e153a4fcd1d53e5e355dbf1e051d612
Denys Malykhin <malykhin@ukr.net>
2018-11-05 22:08:45 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DOOR_SENSOR_PIN: 11,
+  INTERVAL: 3000,
+  REPORT_URL: 'http://192.168.1.23:3000',
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict'
+
+require('isomorphic-fetch')
+const util = require('util')
+const gpio = require('rpi-gpio')
+const sensor = require('ds18b20-raspi')
+
+const config = require('./config')
+
+const gpiop = gpio.promise
+const readSimpleC = util.promisify(sensor.readSimpleC)
+
+let isDoorOpen = false
+
+gpiop
+  .setup(config.DOOR_SENSOR_PIN, gpio.DIR_IN)
+  .then(() => gpiop.read(config.DOOR_SENSOR_PIN))
+  .then((value) => {
+    isDoorOpen = value
+    return gpiop.setup(config.DOOR_SENSOR_PIN, gpio.DIR_IN, gpio.EDGE_BOTH)
+  })
+  .then(() => {
+    gpio.on('change', (channel, value) => {
+      if (channel === config.DOOR_SENSOR_PIN) {
+        isDoorOpen = value
+      }
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
+
+function tick() {
+  readSimpleC()
+    .then((temp) => {
+      const data = {
+        temp,
+        isDoorOpen,
+        timestamp: new Date().toISOString(),
+      }
+
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      return fetch(config.REPORT_URL, options)
+    })
+    .then((response) => {
+      if (response.status >= 400) {
+        console.log('Bad response')
+      }
+      return response.json()
+    })
+    .then((response) => {
+      console.log('Response: ', response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+setInterval(tick, config.INTERVAL)
